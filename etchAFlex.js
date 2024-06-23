64d9f0ed788d81aea48525375ed14cac3f3cd4aa
Chris Skeens <cskee004@odu.edu>
2024-06-23 16:27:42 -0400

    Initial commit

--- etchAFlex.js
+++ etchAFlex.js
@@ -1,14 +1,11 @@
-// Goal 1: Implement a box that changes colors and dims on mouse hover
-// Goal 2: Implement that same box but only using javascript
-
 const container = document.querySelector("#container");
 console.log(container);
 
 container.addEventListener("mouseover", (event) => {
-    // highlight the mouseenter target
+    
     event.target.style.backgroundColor = "black"
 
-    // reset the color after a short delay
+   
     setTimeout(() => {event.target.style.backgroundColor = "white";}, 1000);
   },
   false,
