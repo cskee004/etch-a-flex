b997709b69a8b17c6241062f418d2f980ee5596f
Chris Skeens <cskee004@odu.edu>
2024-06-24 05:48:45 -0400

    jsdoc

--- etchAFlex.js
+++ etchAFlex.js
@@ -1,3 +1,10 @@
+// Goal 1: Implement a set of boxes that change color on mouse over events
+// Goal 2: Implement a method that takes two non-negative integers as input from keyboard
+// Goal 3: Implement a method that creates n amount of individual flex boxes. 
+// Goal 4: 
+
+
+
 const container = document.querySelector("#container");
 console.log(container);
 
